d27168dabaf68c71142fc70cc93ca901223ad9a8
devvratmiglani <devvratmiglani@gmail.com>
2024-01-12 16:18:22 +0530

    Initial repository commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="vm-cli",
+    version="1.0",
+    description="CLI tool to search and download roms from vimm's lair",
+    url="https://github.com/devvratmiglani",
+    author="Devvrat Miglani",
+    author_email="devvratmiglani@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=["vm"],
+    install_requires=[
+        "Click",
+        "beautifulsoup4",
+        "requests",
+        "tabulate",
+    ],
+    entry_points="""
+        [console_scripts]
+        vm=vm:cli
+    """,
+)
